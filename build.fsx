21dcc63958c96a4fc65146d4b329ea9b1612fee5
Timothy McLane <timothyamclane@gmail.com>
2019-10-16 16:11:56 -0500

    Adding .gitignore and build script

---
+++ build.fsx
@@ -0,0 +1,52 @@
+let projectName = "ArkadiusTradeTools"
+let archiveName = "arkadius-trade-tools"
+let directoryGlob = sprintf "build/%s/" projectName
+
+open Fake.IO.Globbing
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.IO.Zip //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.IO
+open Fake.IO.Globbing.Operators
+
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target.create "Clean" (fun _ ->
+  Directory.ensure buildDir
+  Shell.cleanDir buildDir
+)
+Target.create "Copy" (fun _ -> 
+  !! "**/*.lua"
+  ++ "LICENSE"
+  ++ "**/*.txt"
+  ++ "**/*.xml"
+  ++ "**/*.md"
+  ++ "**/*.dds"
+  -- "**.fake/**/*"
+  |> GlobbingPattern.setBaseDir "./"
+  |> Shell.copyFilesWithSubFolder directoryGlob
+)
+
+Target.create "Deploy" (fun p ->
+  (sprintf "%s-%s.zip" archiveName p.Context.Arguments.Head, !! (sprintf "%s/**" directoryGlob))
+  ||> Zip.zip "build/"
+  Shell.cleanDir buildDir
+)
+
+Target.create "Default" ignore
+
+// Dependencies
+open Fake.Core.TargetOperators
+
+"Clean"
+  ==> "Copy"
+  ==> "Default"
+  ==> "Deploy"
+
+// start build
+Target.runOrDefaultWithArguments "Default"
