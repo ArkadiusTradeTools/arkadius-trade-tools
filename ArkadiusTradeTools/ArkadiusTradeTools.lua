0eaeb0001b59f73ca610f26bcb3530772df60060
Timothy McLane <timothyamclane@gmail.com>
2019-10-16 17:14:42 -0500

    v1.1.3

--- ArkadiusTradeTools/ArkadiusTradeTools.lua
+++ ArkadiusTradeTools/ArkadiusTradeTools.lua
@@ -1,7 +1,7 @@
 ArkadiusTradeTools = ZO_CallbackObject:New()
 ArkadiusTradeTools.NAME = 'ArkadiusTradeTools'
 ArkadiusTradeTools.TITLE = 'Arkadius Trade Tools'
-ArkadiusTradeTools.VERSION = '1.1.3-alpha1'
+ArkadiusTradeTools.VERSION = '1.1.3'
 ArkadiusTradeTools.AUTHOR = '@Aldanga, @Arkadius1'
 ArkadiusTradeTools.Localization = {}
 ArkadiusTradeTools.SavedVariables = {}
