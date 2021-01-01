9f6b4a18eed244f2261b0ddc4ef2070ce7c52da2
Timothy McLane <timothyamclane@gmail.com>
2020-12-31 22:18:51 -0600

    Updating CLI options to support flags and new values

--- ArkadiusTradeToolsExports/ConvertToCSV.lua
+++ ArkadiusTradeToolsExports/ConvertToCSV.lua
@@ -1,22 +1,27 @@
-IN_FILE_PATH  = "../../SavedVariables/ArkadiusTradeToolsExports.lua"
+IN_FILE_PATH  = "..\\..\\SavedVariables\\ArkadiusTradeToolsExports.lua"
 dofile(IN_FILE_PATH)
 
-local args = {
-    ['--latest'] = 'useLatest',
-    ['--members'] = 'useOnlyMembers'
-}
+local cli = require "cliargs"
 
--- Defining these as globals so setting them by name is easier
-useLatest = false
-useOnlyMembers = false
+cli:set_name('Arkadius Trade Tools Exports - Convert to CSV')
+cli:flag('-l, --latest', 'Use the latest export', false)
+cli:flag('-m, --members', 'Export only members', false)
+cli:option('-d DIRECTORY, --directory=DIRECTORY', 'Output directory (no trailing slash)', '..\\..\\SavedVariables')
+cli:option('-o OUTFILE, --out-file=OUTFILE', 'Output filename', 'ArkadiusTradeToolsExports-guildName isMembers startTimeStamp-endTimeStamp')
+cli:option('-f FORMAT, --date-format=FORMAT', 'Date format to use for the file name', "%Y-%m-%dT%H.%M.%S")
 
-for idx, _arg in pairs(arg) do
-    if args[_arg] then
-        _G[args[_arg]] = true
-    end
+local args, err = cli:parse(arg)
+if err then 
+    print(err)
+    os.exit(1)
 end
+local useLatest = args.l
+local useOnlyMembers = args.m
+local dateFormat = args.f
+local outFile = args.o
+local directory = args.d
 
-local useLatest = arg[1] and arg[1] == '--latest'
+print(outFile)
 
 local function escapeQuotes(s)
     local value = s:gsub("\"", "\"\"")
@@ -64,22 +69,6 @@ while not isValidServer(input) do
     io.flush()
     input = tonumber(io.read())
 end
-
--- local guilds = {}
--- for k, v in pairs(ArkadiusTradeToolsExportsData.exports[servers[input]]) do
---     guilds[v.guildName] = true
--- end
-
--- local guildName
--- repeat
---     io.write('Available Servers:\n')
---     for key, value in pairs(servers) do
---         io.write(key .. ". " .. value .. "\n")
---     end
---     io.write("Which server would you like to use? ")
---     io.flush()
---     guildName = tonumber(io.read())
--- until isValidServer(guildName)
 
 local exports = ArkadiusTradeToolsExportsData.exports[servers[input]]
 table.sort(exports, function(a, b)
@@ -146,13 +135,13 @@ local headersMap = {
     "Sold Items",
 }
 local headers = table.concat(headersMap, ',') .. '\n'
-
-local startTimeStamp = os.date('%Y-%m-%dT%H.%M.%S', sortedExportsData.startTimeStamp)
-local endTimeStamp = os.date('%Y-%m-%dT%H.%M.%S', sortedExportsData.endTimeStamp)
+local startTimeStamp = os.date(dateFormat, sortedExportsData.startTimeStamp)
+local endTimeStamp = os.date(dateFormat, sortedExportsData.endTimeStamp)
 local isMembers = useOnlyMembers and "Members" or "All"
-local newPath = string.format("../../SavedVariables/ArkadiusTradeToolsExports-%s %s %s-%s.csv", sortedExportsData.guildName, isMembers, startTimeStamp, endTimeStamp)
+local outFileName = outFile:gsub('guildName', sortedExportsData.guildName):gsub('isMembers', isMembers):gsub('startTimeStamp', startTimeStamp):gsub('endTimeStamp', endTimeStamp)
+local newPath = string.format("%s\\%s.csv", directory, outFileName)
 
-OUT_FILE_PATH = "../../SavedVariables/ArkadiusTradeToolsExports.csv"
+OUT_FILE_PATH = "..\\..\\SavedVariables\\ArkadiusTradeToolsExports.csv"
 OUT_FILE = assert(io.open(newPath, "w"))
 OUT_FILE:write(headers)
 
--- ArkadiusTradeToolsExports/README.md
+++ ArkadiusTradeToolsExports/README.md
@@ -1,11 +1,22 @@
 # Arkadius' Trade Tools Exports for Elder Scrolls Online
 
 ## Running exports
-After saving an export within ESO, you can generate a CSV file from your system by running the `run-export.bat` file in the root of the addon.
+After saving an export within ESO, you can generate a CSV file from your system by running the `run-export.bat` or `run-export.ps1` files in the root of the addon.
 
 ## Options
-The export script supports two option flags, but more may be added in the future:
+The export script supports the following options:
 
-`--latest`: Automatically picks the newest saved export for generation.
+`-l, --latest`: Automatically picks the newest saved export for generation.
 
-`--members`: Includes only guild members when generating the export file.
+`-m, --members`: Includes only guild members when generating the export file.
+
+`-d, --directory`: Sets the output directory for the generated CSV. The following values can be used to generate dynamic file names:
+
+- guildName
+- isMembers
+- startTimeStamp
+- endTimeStamp 
+
+`-o, --out-file`: Sets the output file name for the generated CSV.
+
+`-f, --date-format`: Sets the [Lua date format](https://www.lua.org/pil/22.1.html) to be used for any timestamps in the file name.
